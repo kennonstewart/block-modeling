ce31b04c939b773dcfb583c966a0bcf726dc7f52
Kennon Stewart <kennon.stewart@gmail.com>
2025-03-26 18:11:55 +0000

    renaming the test_base_sbm file

--- src/sbm/base_sbm.py
+++ src/sbm/base_sbm.py
@@ -5,14 +5,12 @@ class BaseSBM:
     Base class for Stochastic Block Model
     """
 
-    def __init__(self, adjacency_matrix: np.ndarray, number_of_blocks):
-        self.adjacency_matrix = adjacency_matrix  # Adjacency matrix
-        self.number_of_blocks = number_of_blocks  # Number of blocks
+    def __init__(self, adjacency_matrix: np.ndarray, number_of_blocks, degree_correction = False):
+        self.adjacency_matrix = adjacency_matrix  # adjacency matrix
+        self.number_of_blocks = number_of_blocks  # number of classes
         self.n = adjacency_matrix.shape[0]
         self.group_assignments = np.random.randint(number_of_blocks, size=self.n)
-
-    def compute_likelihood(self):
-        raise NotImplementedError
+        self.degree_correction = degree_correction
 
     def fit(self):
         raise NotImplementedError
